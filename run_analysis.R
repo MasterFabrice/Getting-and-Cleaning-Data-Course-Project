b632b67784761664755956507be85c80ff24ffff
MasterFabrice <fabrice.vouillamoz@icloud.com>
2017-03-14 21:42:06 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,14 +19,14 @@ dat_training <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 str(dat_training)
 # Extract activities information
 dat_name <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-colnames(dat_name) <- c("Activity_ID", "Activity_Name")
+colnames(dat_name) <- c("Activity_ID", "Activity")
 dat <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 str(dat)
 # Merge Participants and Activities
 dat_training <- cbind(dat_training,dat)
 colnames(dat_training) <- c("Subject_ID", "Activity_ID")
 total <- merge(dat_training,dat_name,by="Activity_ID") %>%
-   select(Subject_ID, Activity_Name)
+   select(Subject_ID, Activity)
 str(total)
 # Extract the measurements
 dat_train_m <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
@@ -37,8 +37,11 @@ dat_train_ms <- dat_train_m[ ,c(1:6,41:46,81:86,121:126,161:166,201,202,214,215,
 str(dat_train_ms)
 data_t <- cbind(total,dat_train_ms)
 training <- data_t %>%
-  gather(measurement, value, -(Subject_ID:Activity_Name)) %>%
-  separate(col = measurement, into = c("Measure_type","Stats", "Axis"))
+  gather(measurement, value, -(Subject_ID:Activity)) %>%
+  separate(col = measurement, into = c("Feature","Stats", "Axis"), convert = TRUE)
+training[,3] <- as.factor(training[,3])
+training[,4] <- as.factor(training[,4])
+training[,5] <- as.factor(training[,5])
 # add a temporary row_ID to fixe issue by spreading the Stats
 training$row <- 1:nrow(total)
 training <- spread(training, Stats, value) 
@@ -52,14 +55,14 @@ dat_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 str(dat_test)
 # Extract activities information
 dat_name <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-colnames(dat_name) <- c("Activity_ID", "Activity_Name")
+colnames(dat_name) <- c("Activity_ID", "Activity")
 dat <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 str(dat)
 # Merge Participants and Activities
 dat_test <- cbind(dat_test,dat)
 colnames(dat_test) <- c("Subject_ID", "Activity_ID")
 total <- merge(dat_test,dat_name,by="Activity_ID") %>%
-  select(Subject_ID, Activity_Name)
+  select(Subject_ID, Activity)
 str(total)
 # Extract the measurements
 dat_test_m <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
@@ -70,8 +73,13 @@ dat_test_ms <- dat_test_m[ ,c(1:6,41:46,81:86,121:126,161:166,201,202,214,215,22
 str(dat_test_ms)
 data_t <- cbind(total,dat_test_ms)
 test <- data_t %>%
-  gather(measurement, value, -(Subject_ID:Activity_Name)) %>%
-  separate(col = measurement, into = c("Measure_type","Stats", "Axis"))
+  gather(measurement, value, -(Subject_ID:Activity)) %>%
+  separate(col = measurement, into = c("Feature","Stats", "Axis"), convert = TRUE)
+test[,3] <- as.factor(test[,3])
+test[,4] <- as.factor(test[,4])
+test[,5] <- as.factor(test[,5])
+str(test)
+
 # add a temporary row_ID to fixe issue by spreading the Stats
 test$row <- 1:nrow(total)
 test <- spread(test, Stats, value) 
@@ -82,17 +90,12 @@ DataSet <- rbind(training, test)
 # STEP 4
 # CREATE THE DATASET WITH AVERAGE BY GROUPED VARIABLE
 DataSet_small <- DataSet %>%
-   group_by(Subject_ID, Activity_Name, Measure_type, Axis) %>%
+   group_by(Subject_ID, Activity, Feature, Axis) %>%
    summarize(Average = mean(mean), Average_Std = mean(std)) %>%
    print
 
 View(DataSet_small) 
 
 write.table(DataSet_small, file = "./data/Submission_Week4.txt",  row.name=FALSE) 
-
-
-
-
-
 
 
